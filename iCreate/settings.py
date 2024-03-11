cb7a57ec700aa24a26e7c115b3fb59c1fc5b374d
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-11 13:05:46 +0000

    Add: variables SITE ID-LOGIN-LOGOUT REDIRECT URL in settings.py

--- iCreate/settings.py
+++ iCreate/settings.py
@@ -46,8 +46,12 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    
+
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
