4ce8f446149e90e4bd10c463381fd733b59a3c25
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-11 20:05:25 +0000

    Add: secret key in env.py-change in settings.py

--- iCreate/settings.py
+++ iCreate/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 if os.path.isfile('env.py'):
-    import env
+    import env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z#qw9)v_rz0e#-2&a1n($f=o41bm*)sj-gr9x=8isakrmuvly7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ 
