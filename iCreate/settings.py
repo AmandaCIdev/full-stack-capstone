d05a727d76a7e5f2ed7b9417e606f0c6cbf345f2
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-11 12:47:45 +0000

    fix: resolve issue syntax if statement-settings.py

--- iCreate/settings.py
+++ iCreate/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
-is os.path.isfile('env.py'):
+if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
