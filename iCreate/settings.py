8e9024b977e25b991b7d94484d6097a3f3351797
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-14 13:08:56 +0000

    Link files to templates directory in heroku-settings.py

--- iCreate/settings.py
+++ iCreate/settings.py
@@ -33,7 +33,7 @@ cloudinary.config(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = BASE_DIR / 'templates'
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
