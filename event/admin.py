d0a8eb80a4df83366e2403410d7f78d85a69f500
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-15 15:03:22 +0000

    Add: update admin-event app

--- event/admin.py
+++ event/admin.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Event
 
 # Register your models here.
 admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'time', 'location']
+    
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        messages.success(request, 'Event saved successfully!')
 
 
