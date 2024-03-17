44cef82fffeeb79ff776508e94858da2ec16b0c7
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-17 02:49:46 +0000

    Fix: apps and templates

--- about/urls.py
+++ about/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from . import views
 
 app_name = 'about'  # Example namespace for the 'about' app
 
 urlpatterns = [
     path('', views.about_site, name='about'),  # URL pattern
 ]
+
+
+
--- event/forms.py
+++ event/forms.py
@@ -6,3 +6,8 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'image', 'date', 'time', 'location', 'description', 'speaker']
+
+        
+class ReviewsForm(forms.Form):
+    author = forms.CharField(max_length=100)
+    body = forms.CharField(widget=forms.Textarea)
