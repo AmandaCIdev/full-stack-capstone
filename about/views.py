3a54a49cf6a4070e0767fc88c60649916b4fdc8d
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-19 06:18:15 +0000

    Fix: views-about app

--- about/views.py
+++ about/views.py
@@ -1,32 +1,32 @@
 # views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib import messages
 from .models import About
 from .forms import SubmitDetailsForm
 
 def about_site(request):
+    
     about = About.objects.order_by('updated_on').first()
-    submit_details_form = SubmitDetailsForm()
 
     if request.method == "POST":
+        print("Received a POST request")
         submit_details_form = SubmitDetailsForm(data=request.POST)
         if submit_details_form.is_valid():
             submit_details_form.save()
-            messages.success(request, "Your details have been received! I endeavor to respond within 2 working days.")
-            return redirect('about')
+            messages.add_message(request, messages.SUCCESS, "Your details have been received! I endeavor to respond within 2 working days.")
     
+    submit_details_form = SubmitDetailsForm()
+
+    print("About to render template")
+
     return render(
         request,
         "about/about.html",
         {'about': about, "submit_details_form": submit_details_form},
+
     )
 
-def about(request):
-    if request.method == 'POST':
-        form = SubmitDetailsForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('success_page')
-    else:
-        form = SubmitDetailsForm()
-    return render(request, 'about.html', {'form': form})
+
+
+# Create your views here.
