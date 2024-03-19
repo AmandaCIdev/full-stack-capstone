4a4c4000cd15da719beb98309f6efbfdd54775b0
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-19 05:22:37 +0000

    Add: views.py-events

--- event/views.py
+++ event/views.py
@@ -4,6 +4,7 @@ Views.py
 # pylint: disable=locally-disabled, no-member
 
 from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.http import HttpResponse
 from django.views import View
 from django.contrib import messages
 from django.http import HttpResponseRedirect
