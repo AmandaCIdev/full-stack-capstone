3d0f1b0008b7e84fb9e24601b32d0316cc0a95d7
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-21 10:25:03 +0000

    modify: DEBUG

--- iCreate/settings.py
+++ iCreate/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEBUG' in os.environ 
 
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io','https://*.herokuapp.com']
