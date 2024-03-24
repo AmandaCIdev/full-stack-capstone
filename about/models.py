6e90ea131bafda1b75995317935fcb18311b7b78
AmandaCIdev M <amandamascurinedev@gmail.com>
2024-03-24 23:55:11 +0000

    Add: readme md and assets-testing

--- about/models.py
+++ about/models.py
@@ -2,25 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 
 from django.db import models
 # Create your models here.
 
 
 class About(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=100, unique=True)
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.title
 
+
 class SubmitDetails(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
     read = models.BooleanField(default=False)
     phone_number = models.IntegerField()
+
     def __str__(self):
-        return f"Submit your details to advertise a service - request from {self.name}"
+        return f"Submit your details - request from {self.name}"
